ce7c17c33de81b9668ea6b464cae47723b66d43d
imalkadilakshan99 <imalkadilakshan99@gmail.com>
2024-11-21 16:18:48 +0530

    Start project

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,11 @@
+import "../Style/NavBar.css"
+
+function NavBar(){
+    return(
+        <div>
+            <h1 className="hello">Helloooooooo</h1>
+        </div>
+    )
+}
+
+export default NavBar;
